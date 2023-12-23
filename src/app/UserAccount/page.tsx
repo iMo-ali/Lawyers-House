6ad2bbb741368acb3ce5c9bc6b8e75bc654f5d10
iMo-ali <61813980+iMo-ali@users.noreply.github.com>
2023-12-23 14:58:46 +0800

    Rework of the script for the user account page
    -Fix of the JavaScript now it works on very reload of the page
    - Added Simple nav bar
    ----
    issues
    -the css have some conflicts

--- src/app/UserAccount/page.tsx
+++ src/app/UserAccount/page.tsx
@@ -1,11 +1,36 @@
-import Link from "next/link";
-import Script from "next/script";
+"use client";
+import "./User.css";
+import { useEffect } from "react";
+import Navbar from "../components/Navbar";
 
 export default function User() {
+  {
+    /* This is The Script to handel the class "switch" */
+  }
+  useEffect(() => {
+    const container = document.getElementById("container");
+    const registerBtn = document.getElementById("register");
+    const loginBtn = document.getElementById("login");
+
+    const handleRegisterClick = () => {
+      container.classList.add("active");
+    };
+
+    const handleLoginClick = () => {
+      container.classList.remove("active");
+    };
+
+    registerBtn.addEventListener("click", handleRegisterClick);
+    loginBtn.addEventListener("click", handleLoginClick);
+
+    return () => {
+      registerBtn.removeEventListener("click", handleRegisterClick);
+      loginBtn.removeEventListener("click", handleLoginClick);
+    };
+  }, []); // Empty dependency array ensures the effect runs once on mount
+
   return (
     <>
-      <Script src="/user.js" />{" "}
-      {/* This is The Script to handel the class "switch" */}
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link
@@ -13,90 +38,84 @@ export default function User() {
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
       />
       <title>User Account</title>
-      <div className="container" id="container">
-        <div className="form-container sign-up">
-          <form>
-            <h1>Create Account</h1>
-            <div className="social-icons">
-              <a href="#" className="icon">
-                <i className="fa-brands fa-google-plus-g" />
-              </a>
-              <a href="#" className="icon">
-                <i className="fa-brands fa-facebook-f" />
-              </a>
-              <a href="#" className="icon">
-                <i className="fa-brands fa-github" />
-              </a>
-              <a href="#" className="icon">
-                <i className="fa-brands fa-linkedin-in" />
-              </a>
-            </div>
-            <span>or use your email for registeration</span>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button>Sign Up</button>
-          </form>
-        </div>
-        <div className="form-container sign-in">
-          <form>
-            <h1>Sign In</h1>
-            <div className="social-icons">
-              <a href="#" className="icon">
-                <i className="fa-brands fa-google-plus-g" />
-              </a>
-              <a href="#" className="icon">
-                <i className="fa-brands fa-facebook-f" />
-              </a>
-              <a href="#" className="icon">
-                <i className="fa-brands fa-github" />
-              </a>
-              <a href="#" className="icon">
-                <i className="fa-brands fa-linkedin-in" />
-              </a>
-            </div>
-            <span>or use your email password</span>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <a href="#">Forget Your Password?</a>
-            <button>Sign In</button>
-          </form>
-        </div>
-        <div className="toggle-container">
-          <div className="toggle">
-            <div className="toggle-panel toggle-left">
-              <h1>Welcome Back!</h1>
-              <p>Hello Old Timer! we welcome you back</p>
-              <p>Just click bellow to go to the login form</p>
-              <button className="hidden" id="login">
-                Sign In
-              </button>
-            </div>
-            <div className="toggle-panel toggle-right">
-              <h1>Hello, Welcome!</h1>
-              <p>
-                New Face!! Register now to get access to the website site
-                features
-              </p>
-              <br />
-              <p>Just click bellow to go to the registeration form</p>
-              <button className="hidden" id="register">
-                Sign Up
-              </button>
+      <body className="user-container">
+        <Navbar />
+
+        <div className="container" id="container">
+          <div className="form-container sign-up">
+            <form>
+              <h1>Create Account</h1>
+              <div className="social-icons">
+                <a href="#" className="icon">
+                  <i className="fa-brands fa-google-plus-g" />
+                </a>
+                <a href="#" className="icon">
+                  <i className="fa-brands fa-facebook-f" />
+                </a>
+                <a href="#" className="icon">
+                  <i className="fa-brands fa-github" />
+                </a>
+                <a href="#" className="icon">
+                  <i className="fa-brands fa-linkedin-in" />
+                </a>
+              </div>
+              <span>or use your email for registration</span>
+              <input type="text" placeholder="Name" />
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <button>Sign Up</button>
+            </form>
+          </div>
+          <div className="form-container sign-in">
+            <form>
+              <h1>Sign In</h1>
+              <div className="social-icons">
+                <a href="#" className="icon">
+                  <i className="fa-brands fa-google-plus-g" />
+                </a>
+                <a href="#" className="icon">
+                  <i className="fa-brands fa-facebook-f" />
+                </a>
+                <a href="#" className="icon">
+                  <i className="fa-brands fa-github" />
+                </a>
+                <a href="#" className="icon">
+                  <i className="fa-brands fa-linkedin-in" />
+                </a>
+              </div>
+              <span>or use your email password</span>
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <a href="#">Forget Your Password?</a>
+              <button>Sign In</button>
+            </form>
+          </div>
+          <div className="toggle-container">
+            <div className="toggle">
+              <div className="toggle-panel toggle-left">
+                <h1>Welcome Back!</h1>
+                <p>Hello Old Timer! we welcome you back</p>
+                <p>Just click bellow to go to the login form</p>
+                <button className="hidden" id="login">
+                  Sign In
+                </button>
+              </div>
+              <div className="toggle-panel toggle-right">
+                <h1>Hello, Welcome!</h1>
+                <p>
+                  New Face!! Register now to get access to the website site
+                  features
+                </p>
+                <br />
+                <p>Just click bellow to go to the registration form</p>
+                <button className="hidden" id="register">
+                  Sign Up
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <br />
-      <br />
-      <Link
-        href="/"
-        className="flex flex-col items-center justify-between p-20 bg-red-800 border-8">
-        Back To Home
-      </Link>
-      {/* This is used as a PLACEHOLDER to navigate the page */}
-      <br />
-      <br />
+      </body>
     </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
 import Link from "next/link";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-      <Link href="/UserAccount" className="flex flex-col items-center justify-between p-10 bg-red-800 border-8">User accounts</Link> /* This is used as a PLACEHOLDER to navigate the page */
+    <>
+      <Navbar />
+      <h1>This is the dashboard</h1>
+    </>
   );
 }
