25186f2e0265427e599f9b0279a04f7644ee83de
iMo-ali <61813980+iMo-ali@users.noreply.github.com>
2024-01-13 15:19:08 +0800

    Small "Fix"

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
           className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 bg-gray-400 z-50">
           <li>
             <Link
-              href="/home"
+              href="/Home-Page"
               className="block py-2 px-4 text-gray-800 rounded hover:bg-blue-500 hover:text-white">
               Home Page
             </Link>
