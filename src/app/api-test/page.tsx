994f0032e6f2397d4ccf07c5e7527cbdbd4a385d
iMo-ali <61813980+iMo-ali@users.noreply.github.com>
2024-01-13 20:04:02 +0800

    API testing
    - a script to fetch data from the api

---
+++ src/app/api-test/page.tsx
@@ -0,0 +1,19 @@
+//THIS IS A TEST FOR API DATA FETCHING
+
+// You can use This json placeholder api for testing "https://jsonplaceholder.typicode.com/posts"
+
+export default async function Home() {
+  const response = await fetch("http://localhost:8000/all-lawyers");
+  const data = await response.json();
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {data.map((lawyer: { title: string; body: string; id: string }) => (
+        <div key={lawyer.id}>
+          <h1>{lawyer.title}</h1>
+          <p>{lawyer.body}</p>
+          <br />
+        </div>
+      ))}
+    </main>
+  );
+}
