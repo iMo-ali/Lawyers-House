88ce108dd867378ebc9780b03a85d14e77fa26a0
iMo-ali <61813980+iMo-ali@users.noreply.github.com>
2024-01-15 14:14:39 +0800

    API Fetching and data display
    a fully working data fetching for BackEnd

--- src/app/api-test/page.tsx
+++ src/app/api-test/page.tsx
@@ -1,18 +1,25 @@
-//THIS IS A TEST FOR API DATA FETCHING
+"use client";
+import React, { useEffect, useState } from "react";
 
-// You can use This json placeholder api for testing "https://jsonplaceholder.typicode.com/posts"
+export default function Home() {
+  const [data, setData] = useState([]);
 
-export default async function Home() {
-  const response = await fetch("http://localhost:8000/all-lawyers");
-  const data = await response.json();
-  console.log(data);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:8000/all-lawyers");
+      const jsonData = await response.json();
+      setData(jsonData);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {data.map((lawyer: { title: string; body: string; id: string }) => (
-        <div key={lawyer.id}>
-          <h1>{lawyer.title}</h1>
-          <p>{lawyer.body}</p>
-          <br />
+      {data.map((lawyer, index) => (
+        <div key={index}>
+          <p>Email: {lawyer[0]}</p>
+          <p>Password: {lawyer[1]}</p>
         </div>
       ))}
     </main>
